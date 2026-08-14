567ef512f4fa16516f9a13e7bb448eebb2e3895d
Adi002eng <140268936+adi002eng@users.noreply.github.com>
2026-08-14 13:33:57 +0530

    update backend url

--- frontend/app/services/api.ts
+++ frontend/app/services/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://ablespace-task-manager-sjoz.vercel.app/api",
+  baseURL:
+    "https://ablespace-task-manager-sjoz-bbmt7by7s-adi002engs-projects.vercel.app/api",
   headers: {
     "Content-Type": "application/json",
   },
